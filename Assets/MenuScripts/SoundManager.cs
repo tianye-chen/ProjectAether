e62ccacd3100641f6b36e9998fec8c5ad61f5b7e
Derek Chun <derekchun2013@gmail.com>
2023-03-13 23:51:33 -0400

    Added a slider for volume

---
+++ Assets/MenuScripts/SoundManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+   
+    public void ChangeVolume() //Function to change the volume.
+    {
+        AudioListener.volume= volumeSlider.value; //Volume equals to slider value
+        Save(); //Saves volume
+    }
+
+
+    private void Load() //Loads previous saved volume
+    {
+        PlayerPrefs.GetFloat("musicVolume");
+    }
+    private void Save() //Function to save volume 
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume", volumeSlider.value);
+    }
+
+}

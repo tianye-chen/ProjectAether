6bda9f3945bb8122205f3b4108eb24e21893aefc
Derek Chun <derekchun2013@gmail.com>
2023-03-18 16:35:32 -0400

    Pause Menu UI
    ability to pause the game by pressing escape
    Resume Button
    Skills/Option Button with no functionality
    Quit Button which brings you back to startmenu

--- Assets/UIScripts/PauseMenuScript.cs
+++ Assets/UIScripts/PauseMenuScript.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using static System.Net.Mime.MediaTypeNames;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static bool IsPaused = false;
+
+    public GameObject pauseMenuUI;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(IsPaused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+    public void SkillsMenu()
+    {
+        UnityEngine.Debug.Log("Skills");
+    }
+
+    public void OptionsMenu()
+    {
+        UnityEngine.Debug.Log("Options");
+    }
+
+    public void QuitGame()
+    {
+
+        UnityEngine.Debug.Log("Quit");
+        SceneManager.LoadScene("StartScreen");
+        Resume();
     }
 }

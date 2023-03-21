dcc286e0d61118700cfe114369fd4479e8df08da
wanderer1599 <93452983+wanderer1599@users.noreply.github.com>
2023-03-21 01:08:04 -0400

    one more

--- Assets/Healing_Ability.cs
+++ Assets/Healing_Ability.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Healing_Ability", menuName = "Ability/Healing_Ability")]
 public class Healing_Ability : Ability
 {
-
+    [SerializeField]
     private float addHealthAbsolute = 0f, addHealthPercentage = 0f;
     public override IEnumerator Use(CharacterBase character1, CharacterBase character2)
     {

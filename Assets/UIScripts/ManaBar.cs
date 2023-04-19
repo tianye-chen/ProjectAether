0ec7e578c3947d579032c921cca8097b6fa036dc
Derek Chun <derekchun2013@gmail.com>
2023-04-19 19:51:03 -0400

    added ManaBar and Script to control mana

---
+++ Assets/UIScripts/ManaBar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBar : MonoBehaviour
+
+{
+    public Slider slider;
+
+    public void SetMaxMana(float mana)
+    {
+        slider.maxValue = mana;
+        slider.value = mana;
+    }
+
+    public void SetMana(float mana)
+    {
+        slider.value = mana;
+    }
+
+}

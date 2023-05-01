9da5635286187c9a54986390320e678db988133f
Derek Chun <derekchun2013@gmail.com>
2023-04-30 22:20:40 -0400

    Added stat increase to status screen
    Player has abilty to now increase stats

--- Assets/UIScripts/StatIncreaseUI.cs
+++ Assets/UIScripts/StatIncreaseUI.cs
@@ -27,42 +27,63 @@ public class StatIncreaseUI : MonoBehaviour
     void Update()
     {
         SP = Player.GetComponent<LevelSystem>().StatPoint;
-        HP.text =  "HP: " + Player.GetComponent<CharacterBase>().health.ToString();
-        ATK.text =  "ATK: " + Player.GetComponent<CharacterBase>().atk.ToString();
-        DEF.text =  "DEF: " + Player.GetComponent<CharacterBase>().def.ToString();
-        MANA.text =  "MANA: " + Player.GetComponent<CharacterBase>().mana.ToString();
-        SPD.text =  "SPD: " + Player.GetComponent<CharacterBase>().speed.ToString();
-        ACC.text =  "ACC: " + Player.GetComponent<CharacterBase>().accuracy.ToString();
+        HP.text =  "HP: " + Player.GetComponent<CharacterBase>().maxHealth.ToString();
+        ATK.text =  "ATK: " + Player.GetComponent<CharacterBase>().maxAtk.ToString();
+        DEF.text =  "DEF: " + Player.GetComponent<CharacterBase>().maxDef.ToString();
+        MANA.text =  "MANA: " + Player.GetComponent<CharacterBase>().maxMana.ToString();
+        SPD.text =  "SPD: " + Player.GetComponent<CharacterBase>().maxSpeed.ToString();
+        ACC.text =  "ACC: " + Player.GetComponent<CharacterBase>().maxAccuracy.ToString();
         StatPointsText.text = "STAT POINTS: " + Player.GetComponent<LevelSystem>().StatPoint.ToString();
     }
 
+
     public void AddStat(string WhichStat) {
         
-        if(SP >= 0) {
+        if(SP > 0) {
             switch(WhichStat) {
                 case "HP":
                     UnityEngine.Debug.Log("HP");
+                    Player.GetComponent<CharacterBase>().maxHealth += 10;
+                    Player.GetComponent<LevelSystem>().StatPoint -= 1;
                     break;
                 case "ATK":
                     UnityEngine.Debug.Log("ATK");
+                    Player.GetComponent<CharacterBase>().maxAtk += 5;
+                    Player.GetComponent<CharacterBase>().atk = Player.GetComponent<CharacterBase>().maxAtk;
+                    Player.GetComponent<LevelSystem>().StatPoint -= 1;
                     break;
                 case "DEF":
                     UnityEngine.Debug.Log("DEF");
+                    Player.GetComponent<CharacterBase>().maxDef += 5;
+                    Player.GetComponent<CharacterBase>().def = Player.GetComponent<CharacterBase>().maxDef;
+                    Player.GetComponent<LevelSystem>().StatPoint -= 1;
                     break;
                 case "MANA":
                     UnityEngine.Debug.Log("MANA");
+                    Player.GetComponent<CharacterBase>().maxMana += 10;
+                    Player.GetComponent<LevelSystem>().StatPoint -=1;
                     break;
                 case "SPD":
                     UnityEngine.Debug.Log("SPD");
+                    Player.GetComponent<CharacterBase>().maxSpeed += 5;
+                    Player.GetComponent<CharacterBase>().speed = Player.GetComponent<CharacterBase>().maxSpeed;
+                    Player.GetComponent<LevelSystem>().StatPoint -= 1;
                     break;
                 case "ACC":
                     UnityEngine.Debug.Log("ACC");
+                    Player.GetComponent<CharacterBase>().maxAccuracy += 5;
+                    Player.GetComponent<CharacterBase>().accuracy = Player.GetComponent<CharacterBase>().maxAccuracy;
+                    Player.GetComponent<LevelSystem>().StatPoint -= 1;
                     break;
 
                     
             }
         }
+        else {
+            UnityEngine.Debug.Log("You don't have enough skill points");
+        }
     }
+
 
 
 }

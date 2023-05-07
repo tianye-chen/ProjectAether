98dd7394080420609a9a6d32591c1be726851ea9
H3X4C3 <anonapple45@gmail.com>
2023-05-06 20:03:19 -0400

    added inventory system and collectible items

---
+++ Assets/Scripts/Inventory Scripts/InventoryManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject InventoryMenu;
+    private bool menuActivated;
+
+    public ItemSlot[] itemSlot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I) && menuActivated)
+        {
+            Time.timeScale = 1;
+            InventoryMenu.SetActive(false);
+            menuActivated = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.I) && !menuActivated)
+        {
+            Time.timeScale = 0; // pause game when menu is open
+            InventoryMenu.SetActive(true);
+            menuActivated = true;
+        }
+    }
+
+    public void AddItem(string itemName, Sprite sprite, ItemType itemType)
+    {
+        for(int i = 0; i < itemSlot.Length; i++)
+        {
+            if (itemSlot[i].isFull == false)
+            {
+                itemSlot[i].AddItem(itemName, sprite, itemType);
+                return;
+            }
+        }
+    }
+}
+
+public enum ItemType
+{
+    weapon,
+    none,
+    head,
+    body,
+    legs,
+};
